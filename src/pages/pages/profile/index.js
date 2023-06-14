d6f20614f2dbc73bfcea7ee497a5b2831c6c44c1
Mutukundu Mahendra Reddy <reddymahendra52@gmail.com>
2023-06-14 12:05:40 +0530

    deploy bug fix

--- src/pages/pages/profile/index.js
+++ src/pages/pages/profile/index.js
@@ -92,7 +92,6 @@ const Profile = () => {
                     </InputAdornment>
                   )
                 }}
-                // onChange={(e) => setUserEmail(e.target.value)}
               />
             </Grid>
             <Grid item xs={12}>
